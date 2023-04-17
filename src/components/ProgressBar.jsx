fa39fc4463e1fead0f1b4e2d2d772d625a87108c
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2023-04-17 13:32:15 +0200

    fix bug

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import {useScroll, motion, useTransform} from 'framer-motion'
 
 export default function ProgressBar() {
-    const [viewport, setViewport] = useState(innerWidth)
+    const [viewport, setViewport] = useState(innerHeight)
     const { scrollYProgress } = useScroll(0)    
     const scaleX = useTransform(scrollYProgress, [0,1], [0, viewport])
 
