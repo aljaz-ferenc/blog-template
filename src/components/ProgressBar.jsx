4bc0859c0c01ae0af81c700e8ecf9079d8f29f21
aljaz-ferenc <aljaz.ferenc@protonmail.com>
2023-07-12 00:47:28 +0200

    update progress bar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import {useScroll, motion, useTransform} from 'framer-motion'
+import React, { useState } from 'react'
+import {useScroll, motion, useTransform, useSpring} from 'framer-motion'
 
 export default function ProgressBar() {
-    const [viewport, setViewport] = useState(innerHeight)
-    const { scrollYProgress } = useScroll(0)    
-    const scaleX = useTransform(scrollYProgress, [0,1], [0, viewport])
+  const [viewport, setViewport] = useState(innerHeight)
+  const { scrollYProgress } = useScroll(0)    
+  const scaleX = useTransform(scrollYProgress, [0,1], [0, viewport])
+  const spring = useSpring(scaleX)
 
     addEventListener('resize', () => {
         setViewport(innerHeight)
@@ -12,7 +13,7 @@ export default function ProgressBar() {
 
   return <motion.div 
   className="progress-bar"
-  style={{height: scaleX}}
+  style={{height: spring}}
   />;
 }
 
